70c03abf61bc99979e59b38da869c3707fd38dae
laralvesouza <lara.b.souzas@gmail.com>
2025-05-30 09:31:42 -0300

    Add files via upload

---
+++ Calculadora/script.js
@@ -0,0 +1,42 @@
+function soma(){
+
+    var val1 = parseFloat(document.getElementById('val1').value)
+
+    var val2 = parseFloat(document.getElementById('val2').value)
+
+    document.getElementById('result').textContent = val1 + val2;
+
+}
+
+function subtracao(){
+
+    var val1 = parseFloat(document.getElementById('val1').value)
+
+    var val2 = parseFloat(document.getElementById('val2').value)
+
+    document.getElementById('result').textContent = val1 - val2;
+
+}
+
+function divisao(){
+
+    var val1 = parseFloat(document.getElementById('val1').value)
+
+    var val2 = parseFloat(document.getElementById('val2').value)
+
+    document.getElementById('result').textContent = val1 / val2;
+
+}
+ 
+function multiplicacao(){
+
+    var val1 = parseFloat(document.getElementById('val1').value)
+
+    var val2 = parseFloat(document.getElementById('val2').value)
+
+    document.getElementById('result').textContent = val1 * val2;
+
+}
+ 
+console.log
+ 
